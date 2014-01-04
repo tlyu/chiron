67cf288b63463579033d9e69ee901ca82a56c9d9
Alex Dehnert <adehnert@mit.edu>
2014-01-04 02:52:25 -0500

    Auto-restart when chiron crashes

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 export PYTHONIOENCODING=utf8
-zwrite -c chiron -i prod -m "Starting chiron instance... (args: \"$@\")"
-~/chiron/main.py "$@"
-zwrite -c chiron -i prod -m "Finished running chiron instance (args: \"$@\")."
+while true; do
+    zwrite -c chiron -i prod -m "Starting chiron instance... (args: \"$@\")"
+    ~/chiron/main.py "$@"
+    zwrite -c chiron -i prod -m "Finished running chiron instance (args: \"$@\")."
+done
