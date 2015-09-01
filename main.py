0f120613201c175df1bbd1ca2af2f76fa5bbf903
Alex Dehnert <adehnert@mit.edu>
2015-09-01 01:34:52 -0400

    Add -c mirrors, per request by achernya/slz

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def add_default_classes(match_engine):
     match_engine.add_classes([
             'broder-test', 'geofft-test', 'adehnert-test',
             'linerva', 'debathena', 'undebathena', 'consult',
-            'sipb', 'sipb-auto', 'scripts', 'barnowl', 'zephyr-dev', 'xvm', 'chiron',
+            'sipb', 'sipb-auto', 'scripts', 'barnowl', 'zephyr-dev', 'xvm', 'chiron', 'mirrors',
             'geofft', 'lizdenys', 'jdreed', 'axs', 'adehnert', 'achernya', 'kcr', 'jesus', 'nelhage', 'csvoss', 'shulinye',
             'assassin',
             'shank',
